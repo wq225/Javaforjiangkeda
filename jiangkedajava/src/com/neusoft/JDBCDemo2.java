8f8556f2b879e2782c1542e423f369b813cb1d22
jinjianjiao <2202251126wq>
2020-11-18 19:52:38 +0800

    Create JDBCDemo2.java

---
+++ jiangkedajava/src/com/neusoft/JDBCDemo2.java
@@ -0,0 +1,33 @@
+package com.neusoft;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ * JDBC连接mysql示例
+ */
+public class JDBCDemo2 {
+    public static void main(String[] args) throws Exception {
+        //        1、导入驱动jar包, 建立libs文件夹， 右键 add as library
+        //        2、 注册驱动
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        //        3、获取数据库连接对象
+        Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;DatabaseName=jiangkeda;user=sa;password=990225");
+        //        4、定义sql
+        String sql = "insert into account values ('?', '赵六', 888)";
+        //        5、获取数据库连接对象statement
+        Statement stmt = conn.createStatement();
+        //        6、执行sql
+        int i = stmt.executeUpdate(sql);
+        //        7、处理结果
+        if (i == 1) {
+            System.out.println("插入成功");
+        } else {
+            System.out.println("插入失败");
+        }
+        //        8、释放资源
+        stmt.close();
+        conn.close();
+    }
+}
