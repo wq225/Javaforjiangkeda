5dd9e8416b1e2702d723c41531d576787a1135d2
jinjianjiao <2202251126wq>
2020-11-20 14:53:04 +0800

    删除操作
    删除一条记录

--- sell_admin/src/com/neusoft/dao/impl/BusinessDaoImpl.java
+++ sell_admin/src/com/neusoft/dao/impl/BusinessDaoImpl.java
@@ -7,6 +7,7 @@ import com.neusoft.utils.JDBCUtils;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,11 +29,76 @@ public class BusinessDaoImpl  implements BusinessDao {
                 business.setBusinessName(businessName);
                 list.add(business);
             }
-
         } catch (Exception e) {
             e.printStackTrace();
         }
-
         return list;
+    }
+
+    @Override
+    public int saveBusiness(String businessName) {
+        int businessId = 0;
+        // 希望插入一个商家的时候自动给一个默认密码
+        String sql = "insert into business(businessName, password) values (?, '123') ";
+        try {
+            conn = JDBCUtils.getConnection();
+            // 要设置返回自增长的键
+            pst = conn.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+            pst.setString(1, businessName);
+            pst.executeUpdate();
+            // 同时获取自增长的id值  一行一列
+            rs = pst.getGeneratedKeys();
+            if (rs.next()){
+                businessId= rs.getInt(1);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally {
+            JDBCUtils.close(rs, pst, conn);
+        }
+
+        return businessId;
+    }
+
+    /**
+     * 删除商家
+     * businessId 商家id
+     * return 0 代表删除失败；1删除成功
+     * */
+    @Override
+    public int removeBusiness(int businessId) {
+        int result=0;
+        String sql="delete from business where businessId=?";
+        try {
+            conn=JDBCUtils.getConnection();
+            //开启事物
+            conn.setAutoCommit(false);
+            pst=conn.prepareStatement(sql);
+            pst.setInt(1,businessId);
+            result=pst.executeUpdate();
+            conn.commit();
+        } catch (Exception e) {
+            result=0;
+            try {
+                conn.rollback();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+            e.printStackTrace();
+        }finally {
+            JDBCUtils.close(pst,conn);
+        }
+        return result;
+    }
+
+    @Override
+    public int updateBusiness(Business business) {
+        return 0;
+    }
+
+    @Override
+    public Business getBusinessById() {
+        return null;
     }
 }
--- sell_admin/src/com/neusoft/test.java
+++ sell_admin/src/com/neusoft/test.java
@@ -7,10 +7,23 @@ import java.util.List;
 
 public class test {
     public static void main(String[] args) {
-        BusinessDaoImpl dao=new BusinessDaoImpl();
-        List<Business> businessList=dao.listBusiness();
-        for(Business b : businessList){
-            System.out.println(b);
-        }
+        /**获取business所以的商家
+             BusinessDaoImpl dao=new BusinessDaoImpl();
+             List<Business> businessList=dao.listBusiness();
+             for(Business b : businessList){
+             System.out.println(b);
+             }
+         * */
+
+
+        /**添加一条记录
+        BusinessDaoImpl dao = new BusinessDaoImpl();
+        int businessid = dao.saveBusiness("江科大食堂");
+        System.out.println("id" + businessid);*/
+
+
+        //删除一条记录
+        BusinessDaoImpl dao = new BusinessDaoImpl();
+        dao.removeBusiness(10013);
     }
 }
