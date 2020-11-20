4c225efd7c5398940582496267fcd2a6c3fa76d0
jinjianjiao <2202251126wq>
2020-11-20 10:41:22 +0800

    建立dao层并测试
    建立dao层并测试

---
+++ sell_admin/src/com/neusoft/domain/Business.java
@@ -0,0 +1,96 @@
+package com.neusoft.domain;
+
+import java.util.SplittableRandom;
+
+public class Business {
+    private Integer businessId;
+    private String password;
+    private String businessName;
+    private String businessAddress;
+    private String businessExplain;
+    private Double starPrice;
+    private Double deliveryPrice;
+
+    public Business(){
+
+    }
+
+    public Business(Integer businessId, String password, String businessName, String businessAddress, String businessExplain, Double starPrice, Double deliveryPrice) {
+        this.businessId = businessId;
+        this.password = password;
+        this.businessName = businessName;
+        this.businessAddress = businessAddress;
+        this.businessExplain = businessExplain;
+        this.starPrice = starPrice;
+        this.deliveryPrice = deliveryPrice;
+    }
+
+    public Integer getBusinessId() {
+        return businessId;
+    }
+
+    public void setBusinessId(Integer businessId) {
+        this.businessId = businessId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getBusinessName() {
+        return businessName;
+    }
+
+    public void setBusinessName(String businessName) {
+        this.businessName = businessName;
+    }
+
+    public String getBusinessAddress() {
+        return businessAddress;
+    }
+
+    public void setBusinessAddress(String businessAddress) {
+        this.businessAddress = businessAddress;
+    }
+
+    public String getBusinessExplain() {
+        return businessExplain;
+    }
+
+    public void setBusinessExplain(String businessExplain) {
+        this.businessExplain = businessExplain;
+    }
+
+    public Double getStarPrice() {
+        return starPrice;
+    }
+
+    public void setStarPrice(Double starPrice) {
+        this.starPrice = starPrice;
+    }
+
+    public Double getDeliveryPrice() {
+        return deliveryPrice;
+    }
+
+    public void setDeliveryPrice(Double deliveryPrice) {
+        this.deliveryPrice = deliveryPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Business{" +
+                "businessId=" + businessId +
+                ", password='" + password + '\'' +
+                ", businessName='" + businessName + '\'' +
+                ", businessAddress='" + businessAddress + '\'' +
+                ", businessExplain='" + businessExplain + '\'' +
+                ", starPrice=" + starPrice +
+                ", deliveryPrice=" + deliveryPrice +
+                '}';
+    }
+}
