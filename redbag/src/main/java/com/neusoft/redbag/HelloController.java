bbb8552ef26288a399a9c2dac978a0bc87be7f48
jinjianjiao <2202251126wq>
2020-11-25 15:42:12 +0800

    111

--- redbag/src/main/java/com/neusoft/redbag/HelloController.java
+++ redbag/src/main/java/com/neusoft/redbag/HelloController.java
@@ -1,4 +1,12 @@
 package com.neusoft.redbag;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello SpringBoot";
+    }
 }
---
+++ redbag/src/main/java/com/neusoft/redbag/User.java
@@ -0,0 +1,8 @@
+package com.neusoft.redbag;
+
+public class User {
+    private String id;
+    private String username;
+    private String password;
+    private String email;
+}
