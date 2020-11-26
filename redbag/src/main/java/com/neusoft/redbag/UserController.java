1fbcc6928e59c60192dd476a8e2be867084eeea6
jinjianjiao <2202251126wq>
2020-11-26 11:08:16 +0800

    111

--- redbag/src/main/java/com/neusoft/redbag/UserController.java
+++ redbag/src/main/java/com/neusoft/redbag/UserController.java
@@ -1,8 +1,11 @@
 package com.neusoft.redbag;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -10,10 +13,12 @@ import java.util.Optional;
 public class UserController {
     @Autowired
     private UserRepository repository;
+
     @PostMapping()
     public User save(@RequestBody User user){
         return repository.save(user);
     }
+
     @GetMapping("/{id}")
     public User getById(@PathVariable("id")String id){
        Optional<User> optional= repository.findById(id);
@@ -22,8 +27,35 @@ public class UserController {
         //对象::new 创建对象的简写 jdk8以上
        return optional.orElseGet(User::new);
     }
+
     @DeleteMapping("/{id}")
     public void delete(@PathVariable("id")String id){
          repository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public User update(@PathVariable("id")String id,@RequestBody User user){
+        user.setId(id);
+        return repository.save(user);
+    }
+
+//    @GetMapping()
+//    public List<User> listAll(){
+//        return repository.findAll();
+//    }
+
+    /**
+     * 分页查询
+     * @param pageNum  页的开始数 ，默认是从 0开始
+     *
+     * @param pageSize 每页几个   默认每页10条数据
+     * @return
+     */
+    //分页参数 页数和每页多少个
+    @GetMapping("/list")
+    public Page<User> pageQuery(@RequestParam(value="pageNum",defaultValue = "1") Integer pageNum,
+                                @RequestParam(value="pageSize",defaultValue = "5") Integer pageSize){
+         PageRequest pageRequest= PageRequest.of(pageNum-1,pageSize);
+         return repository.findAll(pageRequest);
     }
 }
