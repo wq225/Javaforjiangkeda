649be06d9787dc05609aa6506027e997fe641b34
jinjianjiao <2202251126wq>
2020-11-26 17:28:05 +0800

    elm

---
+++ springbootsell/src/main/java/com/neusoft/springbootsell/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.neusoft.springbootsell.services;
+
+import com.neusoft.springbootsell.dataobject.ProductInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface ProductService {
+    //查询一个商品
+    ProductInfo findOne(String productId);
+    //查询所有商品
+    List<ProductInfo> findUpAll();
+    //分页查询所有商品
+    Page<ProductInfo> findAll(Pageable pageable);
+    //新增商品
+    ProductInfo save(ProductInfo productInfo);
+    //上架商品
+    void increaseStock(String productId);
+    //下架商品
+    void decreaseStock(String productId);
+    //加库存
+
+    //减库存
+}
